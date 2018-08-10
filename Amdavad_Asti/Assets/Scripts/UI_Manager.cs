a52f0808eb1ce729c9c3ae4ca19f402b49f47708
gameboyJ3 <42186306+gameboyJ3@users.noreply.github.com>
2018-08-11 01:11:52 +0530

    UI added

---
+++ Amdavad_Asti/Assets/Scripts/UI_Manager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Manager : MonoBehaviour 
+{
+	public GameObject StartingPanel;
+	public GameObject ModeSeletPanel;
+	public GameObject BetSelectPanel;
+
+	public void Play()
+	{
+		DisableAllPanel ();
+		ModeSeletPanel.SetActive (true);
+	}
+
+	public void SelectMode(GameObject modename)
+	{
+		if(modename.name == "VsComp")
+		{
+			//for now we are opening bet panel for both mode
+			DisableAllPanel();
+			BetSelectPanel.SetActive (true);
+		}
+		if(modename.name == "VsLocal")
+		{
+			//for now we are opening bet panel for both mode
+			DisableAllPanel();
+			BetSelectPanel.SetActive (true);
+		}
+	}
+
+
+	public void DisableAllPanel()
+	{
+		StartingPanel.SetActive (false);
+		ModeSeletPanel.SetActive (false);
+		BetSelectPanel.SetActive (false);
+	}
+}
