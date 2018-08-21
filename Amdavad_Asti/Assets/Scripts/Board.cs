7cdb5caf8ac8367703163caea8c027318c91e2db
gameboyJ3 <42186306+gameboyJ3@users.noreply.github.com>
2018-08-21 14:11:23 +0530

    now we need to add turn manager

--- Amdavad_Asti/Assets/Scripts/Board.cs
+++ Amdavad_Asti/Assets/Scripts/Board.cs
@@ -7,6 +7,7 @@ public class Board : MonoBehaviour {
 
 	public Text showDiceValue;
 	public GameObject[] player1_pawns;
+	public GameObject[] player2_pawns;
 
 	void Update () 
 	{
@@ -18,6 +19,10 @@ public class Board : MonoBehaviour {
 		{
 			player1_pawns [i].GetComponent<Animator> ().enabled = true;
 		}
+		for(int i=0;i<player2_pawns.Length;i++)
+		{
+			player2_pawns [i].GetComponent<Animator> ().enabled = true;
+		}
 	}
 	public void StopPunching()
 	{
@@ -25,6 +30,11 @@ public class Board : MonoBehaviour {
 		{
 			player1_pawns [i].GetComponent<Animator> ().enabled = false;
 			player1_pawns [i].GetComponent<RectTransform> ().sizeDelta = new Vector2 (12,12);
+		}
+		for(int i=0;i<player2_pawns.Length;i++)
+		{
+			player2_pawns [i].GetComponent<Animator> ().enabled = false;
+			player2_pawns [i].GetComponent<RectTransform> ().sizeDelta = new Vector2 (12,12);
 		}
 	}
 }
